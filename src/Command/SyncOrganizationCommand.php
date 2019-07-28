14f627449c8d2be8cc6128d9334a1333487c10dd
Rostyslav Proskuriakov <proskuryakov.r@gmail.com>
2019-07-28 03:54:18 +0200

    Proper configuration, Push organization command

--- src/Command/SyncOrganizationCommand.php
+++ src/Command/SyncOrganizationCommand.php
@@ -2,7 +2,64 @@
 
 namespace App\Command;
 
-class SyncOrganizationCommand
-{
+use LinkORB\OrgSync\SynchronizationMediator\SynchronizationMediatorInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 
+class SyncOrganizationCommand extends Command
+{
+    private const DEFAULT_ORGANIZATION_CONFIG = 'organization.yaml';
+    private const DEFAULT_TARGETS_CONFIG = 'targets.yaml';
+
+    /** @var SynchronizationMediatorInterface */
+    private $synchronizationMediator;
+
+    /** @var string */
+    private $configBasePath;
+
+   public function __construct(SynchronizationMediatorInterface $synchronizationMediator, string $configBasePath)
+    {
+        $this->synchronizationMediator = $synchronizationMediator;
+        $this->configBasePath = $configBasePath;
+
+        parent::__construct();
+    }
+
+    public function configure()
+    {
+        $this->setName('linkorb:organization:sync')
+            ->setDescription('Sync organization data with provided config')
+            ->addOption(
+                'organization',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'The name of organization config',
+                static::DEFAULT_ORGANIZATION_CONFIG
+            )
+            ->addOption(
+                'targets',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'The name of targets config',
+                static::DEFAULT_TARGETS_CONFIG
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $organization = $this->getFromYaml($this->configBasePath . '/' . $input->getOption('organization'));
+        $targets= $this->getFromYaml($this->configBasePath . '/' . $input->getOption('targets'));
+
+        $organization = $this->synchronizationMediator->initialize($targets, $organization);
+
+        $this->synchronizationMediator->pushOrganization($organization);
+    }
+
+    protected function getFromYaml(string $path): array
+    {
+        return Yaml::parseFile($path);
+    }
 }
